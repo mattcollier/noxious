b2e546df1a0fa8a408d1c31f9a0cff46820fcc2e
Matthew Collier <collier.matthew@gmail.com>
2015-03-20 15:16:32 -0400

    fix ths directory

--- nox-main.js
+++ nox-main.js
@@ -14,7 +14,7 @@ var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 var thsBuilder = require('ths');
-var ths = new thsBuilder();
+var ths = new thsBuilder(__dirname);
 // crypto related
 
 var NoxCrypto = require(__dirname + '/nox-crypto.js');
