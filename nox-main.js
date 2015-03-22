badb180e75357c0757cd817cd48e3363af408a07
Matthew Collier <collier.matthew@gmail.com>
2015-03-22 16:49:22 -0400

    more crypto

--- nox-main.js
+++ nox-main.js
@@ -88,6 +88,31 @@ function encrypt(noxCrypto, clearText){
     return Buffer.concat(encryptedBuffersList).toString('base64');
 }
 
+function decrypt(encryptedString){
+    var keySizeBytes = myCrypto.keySize;
+    var encryptedBuffer = new Buffer(encryptedString, 'base64');
+    var decryptedBuffers = [];
+
+    //if the clear text was encrypted with a key of size N, the encrypted
+    //result is a string formed by the concatenation of strings of N bytes long,
+    //so we can find out how many substrings there are by diving the final result
+    //size per N
+    var totalBuffers = encryptedBuffer.length / keySizeBytes;
+
+    //decrypts each buffer and stores result buffer in an array
+    for(var i = 0 ; i < totalBuffers; i++){
+        //copies next buffer chunk to be decrypted in a temp buffer
+        var tempBuffer = new Buffer(keySizeBytes);
+        encryptedBuffer.copy(tempBuffer, 0, i*keySizeBytes, (i+1)*keySizeBytes);
+        //decrypts and stores current chunk
+        var decryptedBuffer = myCrypto.myPrivKey.decrypt(tempBuffer);
+        decryptedBuffers.push(decryptedBuffer);
+    }
+
+    //concatenates all decrypted buffers and returns the corresponding String
+    return Buffer.concat(decryptedBuffers).toString();
+}
+
 function buildEncryptedMessage(destAddress, msgText) {
   // TODO should crypto objects be stored in an array for reuse?
   var tmpPubPEM = contactList.getKey(destAddress).pubPEM;
@@ -209,6 +234,7 @@ function processMessage(msg) {
       break;
     case 'encryptedData':
       console.log('Encrypted Data: ', content.data);
+      console.log('Decrypted Data: ', decrypt(content.data));
       break;
   }
 }
