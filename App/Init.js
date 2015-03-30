57966ad10f74915905912ecdb07d182d0e7395e0
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-30 04:41:15 -0700

    :new: Add Init.js This file will enable the `--es_staging` flag for us in the v8, so we can use our classes in other files

---
+++ App/Init.js
@@ -0,0 +1,7 @@
+
+
+var v8 = require('v8');
+v8.setFlagsFromString('--harmony_classes');
+v8.setFlagsFromString('--harmony_object_literals');
+v8.setFlagsFromString('--harmony_tostring');
+require('./App').Init();
