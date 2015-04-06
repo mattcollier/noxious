884c556b3def2dab12647bec73c51f7734faa703
Matthew Collier <collier.matthew@gmail.com>
2015-04-06 08:56:16 -0400

    use call in windows build script

--- build_win_ia32.bat
+++ build_win_ia32.bat
@@ -1,3 +1,4 @@
 cd node_modules\ursa
-node-gyp rebuild --target=0.22.1 --arch=ia32 --target_platform=win --dist-url=https://atom.io/download/atom-shell
+REM 'call' must be used here because node-gyp calls another batch file
+call node-gyp rebuild --target=0.22.3 --arch=ia32 --target_platform=win --dist-url=https://atom.io/download/atom-shell
 cd ..\..
